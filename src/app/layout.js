a235b3c0ce62dbcb44c9d1b42bbc5eb1c10903f0
ashik081 <freelancerashik81@gmail.com>
2026-04-15 10:10:56 +0600

    added not found page

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,11 +27,7 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ src/app/not-found.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import errorImg from "@/assets/error-404.png";
+
+const NotFoundPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 px-6">
+      <div className="text-center">
+        {/* Decorative Element */}
+        <figure className="flex justify-center items-center">
+          <Image src={errorImg} width={500} height={500} alt="404 page"></Image>
+        </figure>
+
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          Page not found
+        </h1>
+
+        <p className="mt-6 text-base leading-7 text-gray-600">
+          Sorry, we couldn’t find the page you’re looking for. It might have
+          moved or disappeared into the digital void.
+        </p>
+
+        <div className="mt-4 flex items-center justify-center gap-x-6">
+          <Link href="/" className="btn btn-primary">
+            Go back home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
