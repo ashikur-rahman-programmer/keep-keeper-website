4c087eaf43b77d8a2dfafaa5e949087183af5913
ashik081 <freelancerashik81@gmail.com>
2026-04-17 14:53:54 +0600

    fixed fetch data  problem

--- src/components/AllFriends/AllFriends.jsx
+++ src/components/AllFriends/AllFriends.jsx
@@ -1,23 +1,24 @@
-"use client";
+// "use client";
 import React from "react";
-import useFriends from "@/hook/useFriends";
+// import useFriends from "@/hook/useFriends";
 import FriendCard from "./FriendCard";
+import fs from "fs/promises";
+import path from "path";
 
-const AllFriends = () => {
-  // async
-  // const res = await fetch("/friendsData.json");
-  // const friends = await res.json();
-  //ei vabe korle url path vul dekhay .. but localhost add korle deploy a problem tai client side korei data fetch korlam.. direct import koreo kora jay but fetch to samne lagbe tai eta diyei try kori ...
+const AllFriends = async () => {
+  const filePath = path.join(process.cwd(), "public/friendsData.json");
+  const file = await fs.readFile(filePath, "utf-8");
+  const friends = JSON.parse(file);
 
-  const { friends, loading } = useFriends();
+  // const { friends, loading } = useFriends();
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center py-40">
-        <span className="loading loading-spinner text-neutral"></span>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="flex justify-center items-center py-40">
+  //       <span className="loading loading-spinner text-neutral"></span>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="container mx-auto pt-10">
