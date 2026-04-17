a02ba04a7ec9db9ec4b0d373d28cf22da66bee35
ashik081 <freelancerashik81@gmail.com>
2026-04-17 17:01:10 +0600

    fixed cards responsive issues

--- src/components/AllFriends/AllFriends.jsx
+++ src/components/AllFriends/AllFriends.jsx
@@ -11,7 +11,7 @@ const AllFriends = async () => {
   return (
     <div className="container mx-auto pt-10">
       <h3 className="text-3xl font-semibold ">Your Friends</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center gap-6 pt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-6 pt-6">
         {friends.map((friend) => (
           <FriendCard key={friend.id} friend={friend} />
         ))}
