478ea845df88b4e675548de364ce1edd4df469de
ashik081 <freelancerashik81@gmail.com>
2026-04-17 16:51:45 +0600

    checked all functionality after deployment

--- src/components/AllFriends/AllFriends.jsx
+++ src/components/AllFriends/AllFriends.jsx
@@ -1,6 +1,4 @@
-// "use client";
 import React from "react";
-// import useFriends from "@/hook/useFriends";
 import FriendCard from "./FriendCard";
 import fs from "fs/promises";
 import path from "path";
@@ -10,20 +8,10 @@ const AllFriends = async () => {
   const file = await fs.readFile(filePath, "utf-8");
   const friends = JSON.parse(file);
 
-  // const { friends, loading } = useFriends();
-
-  // if (loading) {
-  //   return (
-  //     <div className="flex justify-center items-center py-40">
-  //       <span className="loading loading-spinner text-neutral"></span>
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="container mx-auto pt-10">
       <h3 className="text-3xl font-semibold ">Your Friends</h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pt-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-items-center gap-6 pt-6">
         {friends.map((friend) => (
           <FriendCard key={friend.id} friend={friend} />
         ))}
